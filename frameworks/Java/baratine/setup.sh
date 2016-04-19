f636bc275978eddd6258969aa5eb6cdb41fc3d11
nam <nam@caucho.com>
2016-04-18 23:12:57 -0700

    baratine config fix

--- frameworks/Java/baratine/setup.sh
+++ frameworks/Java/baratine/setup.sh
@@ -4,4 +4,4 @@ fw_depends java maven
 
 mvn clean package
 
-java -jar target/testTechempowerBaratine-0.0.2-SNAPSHOT.jar ${DBHOST}
+java -jar target/testTechempowerBaratine-0.0.2-SNAPSHOT.jar jdbc:mysql://${DBHOST}
