df2c7faf7e7bc1ffb0caaf51475176071ce4671d
nam <nam@caucho.com>
2016-04-19 00:36:23 -0700

    baratine mysql database table

--- frameworks/Java/baratine/setup.sh
+++ frameworks/Java/baratine/setup.sh
@@ -4,4 +4,4 @@ fw_depends java maven
 
 mvn clean package
 
-java -jar target/testTechempowerBaratine-0.0.2-SNAPSHOT.jar "jdbc:mysql://${DBHOST}" benchmarkdbuser benchmarkdbpass
+java -jar target/testTechempowerBaratine-0.0.2-SNAPSHOT.jar jdbc:mysql://${DBHOST}/hello_world benchmarkdbuser benchmarkdbpass
